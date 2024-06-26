83f89b016c2e60808dc4b4d1197e3de50d3560d3
Jpvh03 <jpvh119thboss@gmail.com>
2024-06-26 16:00:45 +0200

    firstGUI

--- ToDoList.java
+++ ToDoList.java
@@ -1,6 +1,32 @@
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.*;
 
-public class ToDoList {
+public class ToDoList extends Application {
+
+    Button button1;
+
     public static void main(String[] args) {
-        System.out.println("HI");
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        arg0.setTitle("This is my title");
+
+        button1 = new Button();
+        button1.setText("Do that");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button1);
+
+        Scene scene1 = new Scene(layout, 300, 250);
+        arg0.setScene(scene1);
+        arg0.show();
+
     }
 }
